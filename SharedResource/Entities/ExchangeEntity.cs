9b5c091ceb309073d5c819a4dd0ed756c8e8e7d1
vilakhanskb <vilakhans@outlook.com>
2025-08-01 14:57:28 +0700

    Exchange

--- SharedResource/Entities/ExchangeEntity.cs
+++ SharedResource/Entities/ExchangeEntity.cs
@@ -8,6 +8,9 @@ namespace SharedResource.Entities
 {
     public class ExchangeEntity
     {
-
+        public string? Currency { get; set; }
+        public double Rate { get; set; }
+        public string? Symbol { get; set; }
+        public StatusAtive Status { get; set; }
     }
 }
