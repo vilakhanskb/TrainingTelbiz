d0837984a77874acda4279a48f8c3bbfa4e36adb
vilakhanskb <vilakhans@outlook.com>
2025-08-04 16:28:59 +0700

    update save

--- CategoryServices/Repository/CategoryRepo.cs
+++ CategoryServices/Repository/CategoryRepo.cs
@@ -1,4 +1,9 @@
-﻿using Couchbase.Extensions.DependencyInjection;
+﻿using Couchbase.Core.Retry;
+using Couchbase.Extensions.DependencyInjection;
+using SharedResource.Config;
+using SharedResource.DTOs;
+using SharedResource.Entities;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CategoryServices.Repository
 {
@@ -12,5 +17,24 @@ namespace CategoryServices.Repository
             this.bucketProvider = bucketProvider;
             this.logger = logger;
         }
+
+        public async Task<bool> Save(CategoryDTOs request)
+        {
+            try
+            {
+                var bucket = await bucketProvider.GetBucketAsync(DBConfig.bucket);
+                var scope = await bucket.ScopeAsync(DBConfig.scope);
+
+                var collection = await scope.CollectionAsync(DBCollection.category);
+                await collection.InsertAsync(request.ID!, request);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
     }
 }
